46dce405f30b10eb820db9bdd8330d805b43a1c0
alekseyC09onl <l.crasutscky2017@yandex.ru>
2021-09-29 08:11:17 +0300

    1

--- Lesson1/src/Task1.java
+++ Lesson1/src/Task1.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-
 public class Task1 {
     public static void main(String[] args) {
-        Scanner klava=new Scanner(System.in);
-        int[] array= {5,9,3,-4,-6,-2,0,8,984};
         int count=0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i]>0){
+        for (String arg : args) {
+            int i= Integer.parseInt(arg.trim());
+            if (i>0){
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Количество положительных чисел = "+count);
     }
 }
 
