128d704ddb37278f70146f387ab647345f2eb0fe
alekseyC09onl <l.crasutscky2017@yandex.ru>
2021-10-17 21:39:38 +0300

    решение ДЗ урока №6 итог2.

--- Lesson6/src/main/java/PersonFactory.java
+++ Lesson6/src/main/java/PersonFactory.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class PersonFactory {
-    private Scanner consoleInt = new Scanner(System.in);
-    private Scanner consoleLine = new Scanner(System.in);
+    private Scanner console = new Scanner(System.in);
     private static int count;
     private Person[] recruit;
     String name;
@@ -16,11 +15,11 @@ public class PersonFactory {
     public Person[] createPerson() {
         System.out.println("Введите количество новобранцев: ");
         while (true) {
-            while (!consoleInt.hasNextInt()) {
+            while (!console.hasNextInt()) {
                 System.out.println("Ошибка при вводе, введите количество новобранцев: ");
-                consoleInt.next();
+                console.next();
             }
-            count = consoleInt.nextInt();
+            count = console.nextInt();
             if (count > 0) {
                 break;
             } else {
@@ -31,14 +30,14 @@ public class PersonFactory {
         for (int i = 0; i < recruit.length; i++) {
             System.out.println("Необходимо заполнить данные " + (i + 1) + "-го новобранца!");
             System.out.println("Введите имя: ");
-            name = consoleLine.nextLine();
+            name = console.next();
             System.out.println("Введите возраст: ");
             while (true) {
-                while (!consoleInt.hasNextInt()) {
+                while (!console.hasNextInt()) {
                     System.out.println("Ошибка при вводе, введите возраст еще раз: ");
-                    consoleInt.next();
+                    console.next();
                 }
-                age = consoleInt.nextInt();
+                age = console.nextInt();
                 if (age > 0) {
                     break;
                 } else {
@@ -47,11 +46,11 @@ public class PersonFactory {
             }
             System.out.println("Выберите пол: \"1\" - мужской, \"2\" - женский.");
             while (true) {
-                while (!consoleInt.hasNextInt()) {
+                while (!console.hasNextInt()) {
                     System.out.println("Ошибка при вводе, повторите попытку: ");
-                    consoleInt.next();
+                    console.next();
                 }
-                sex1 = consoleInt.nextInt();
+                sex1 = console.nextInt();
                 if (sex1 == 1 || sex1 == 2) {
                     if (sex1 == 1) {
                         sex = Person.MALE;
@@ -65,9 +64,9 @@ public class PersonFactory {
 
             }
             System.out.println("Введите страну проживания: ");
-            country = consoleLine.nextLine();
+            country = console.next();
             System.out.println("Введите город проживания: ");
-            city = consoleLine.nextLine();
+            city = console.next();
             recruit[i] = new Person(name, age, sex, country, city);
         }
         return recruit;
