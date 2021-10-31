6dd0c9fbc9aeac78727f40a114975bc8b429298e
alekseyC09onl <l.crasutscky2017@yandex.ru>
2021-10-31 21:07:09 +0300

    Решение ДЗ урока №9 итог!

--- Lesson9/src/main/java/task3/Main.java
+++ Lesson9/src/main/java/task3/Main.java
@@ -3,8 +3,8 @@ package task3;
 public class Main {
     public static void main(String[] args) {
         Cosmodrome cosmodrome = new Cosmodrome();
-//        cosmodrome.start(new SpaceX());
-//        cosmodrome.start(new Shuttle());
+        cosmodrome.start(new SpaceX());
+        cosmodrome.start(new Shuttle());
         cosmodrome.start(new Hiliner());
     }
 }
