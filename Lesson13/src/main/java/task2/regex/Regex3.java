08561492166fdbb154a1e40ca7060fad76018557
alekseyC09onl <l.crasutscky2017@yandex.ru>
2021-11-14 21:57:30 +0300

    Решение ДЗ урока № 13_v2

--- Lesson13/src/main/java/task2/regex/Regex3.java
+++ Lesson13/src/main/java/task2/regex/Regex3.java
@@ -3,6 +3,6 @@ package task2.regex;
 public class Regex3 {
     public static void main(String[] args) {
         String str = "One two three раз два три one1 two2 123 ";
-        System.out.println(str.split("[a-zA-Z]+\s").length - 1);
+        System.out.println(str.split("(\\s)|([а-яА-Я\\s]+)|([0-9\\s]+)|([a-zA-Z]+[0-9]+)").length);
     }
 }
