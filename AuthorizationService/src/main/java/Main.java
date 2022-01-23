893c6744ea5f58611c45632db023187c8a4b64df
alekseyC09onl <l.crasutscky2017@yandex.ru>
2022-01-23 18:31:16 +0300

    Авторизация

--- AuthorizationService/src/main/java/Main.java
+++ AuthorizationService/src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Service service = new Service();
-        service.registrationUser("alex2", "46461", "Alex1", 251);
+//        service.registrationUser("alex2", "46461", "Alex1", 251);
+//        service.loginToAccount("alex", "12345678");
+        service.changeAccountPassword("alex", "11231234qwe");
     }
 }
--- AuthorizationService/src/main/java/Service.java
+++ AuthorizationService/src/main/java/Service.java
@@ -16,13 +16,107 @@ public class Service {
     ResultSet resultSet = null;
     int count = 0;
 
+
     public void registrationUser(String login, String password, String name, int age) {
         try {
-            Statement statement = connection.createStatement(); //("INSERT INTO schema_authorization.users (login, password, firstname, age) VALUE ('alex', '12345', 'Alex', 24);");
+            Statement statement = connection.createStatement();
             count = statement.executeUpdate("INSERT INTO schema_authorization.users (login, password, firstname, age) VALUE ('" + login + "', '" + password + "', '" + name + "', " + age + ");");
             System.out.println("Добавлен " + count + " пользователь: " + login);
         } catch (SQLException e) {
             System.out.println("Ошибка добавления, пользователь " + login + " уже существует!");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void loginToAccount(String login, String password) {
+        String originPassword = null;
+        try {
+            preparedStatement = connection.prepareStatement("select password from users where login = '" + login + "';");
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                originPassword = resultSet.getString(1);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка соединения с базой.");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (originPassword != null) {
+            if (originPassword.equals(password)) {
+                System.out.println("Вход успешно осуществлен");
+            } else {
+                System.out.println("Неверно введен пароль!");
+            }
+        } else {
+            System.out.println("Ошибка, аккаунт " + login + " не существует.");
+        }
+    }
+
+    public boolean checkUserExist(String login) {
+        String tempLogin = null;
+        try {
+            preparedStatement = connection.prepareStatement("select login from users where login = '" + login + "';");
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                tempLogin = resultSet.getString(1);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка соединения с базой.");
+        }
+        if (tempLogin != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getUsersPassword(String login) {
+        String tempPassword = null;
+        if (checkUserExist(login)) {
+            try {
+                preparedStatement = connection.prepareStatement("select password from users where login = '" + login + "';");
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    tempPassword = resultSet.getString(1);
+                }
+            } catch (SQLException e) {
+                System.out.println("Ошибка соединения с базой.");
+            }
+        }
+        return tempPassword;
+    }
+
+
+    public void changeAccountPassword(String login, String newPassword) {
+        if (checkUserExist(login)) {
+            if (getUsersPassword(login).equals(newPassword)) {
+                System.out.println("Новый пароль должен отличаться от старого!");
+            } else {
+                try {
+                    Statement statement = connection.createStatement();
+                    statement.executeUpdate("update users SET password = '" + newPassword + "' where login = '" + login + "';");
+                    System.out.println("Пароль успешно изменен.");
+                } catch (SQLException e) {
+                    System.out.println("Ошибка соединения с базой.");
+                } finally {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } else {
+            System.out.println("Пользователя " + login + " не существует!");
         }
 
     }
